97da42341a940ee50141f3ed936a8907fbb12f56
Sam Hazinski <shazinski@gmail.com>
2024-06-05 14:49:37 -0400

    reworked post display handlebars

--- models/index.js
+++ models/index.js
@@ -3,14 +3,14 @@ const Project = require('./Project');
 const Post = require('./Post')
 
 //get rid of once functional
-User.hasMany(Project, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-});
+// User.hasMany(Project, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+// });
 
-Project.belongsTo(User, {
-  foreignKey: 'user_id'
-});
+// Project.belongsTo(User, {
+//   foreignKey: 'user_id'
+// });
 
 User.hasMany(Post, {
   foreignKey: 'user_id',
